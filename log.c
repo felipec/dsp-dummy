82f1fdbcd27f9631d85df189c339143ad25698d0
Felipe Contreras <felipe.contreras@gmail.com>
2010-05-08 23:34:58 +0300

    Revamp logging stuff
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- log.c
+++ log.c
@@ -1,7 +1,8 @@
 /*
- * Copyright (C) 2009 Nokia Corporation.
+ * Copyright (C) 2009-2010 Felipe Contreras
+ * Copyright (C) 2009-2010 Nokia Corporation
  *
- * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ * Author: Felipe Contreras <felipe.contreras@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,7 +24,28 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+#define SYSLOG
+
+#ifdef SYSLOG
+#include <syslog.h>
+#endif
+
+#if defined(DEBUG)
 unsigned int debug_level = 1;
+#endif
+
+#ifdef SYSLOG
+static inline int
+log_level_to_syslog(unsigned int level)
+{
+	switch (level) {
+	case 0: return LOG_ERR;
+	case 1: return LOG_WARNING;
+	case 2: return LOG_INFO;
+	default: return LOG_DEBUG;
+	}
+}
+#endif
 
 static inline const char *
 log_level_to_string(unsigned int level)
@@ -37,28 +59,40 @@ log_level_to_string(unsigned int level)
 	}
 }
 
-void
-pr_helper(unsigned int level,
-	  const char *file,
-	  const char *function,
-	  unsigned int line,
-	  const char *fmt,
-	  ...)
+void pr_helper(unsigned int level,
+	       const char *file,
+	       const char *function,
+	       unsigned int line,
+	       const char *fmt,
+	       ...)
 {
 	char *tmp;
 	va_list args;
 
+#if defined(DEBUG)
 	if (level > debug_level)
 		return;
+#endif
 
 	va_start(args, fmt);
 
 	vasprintf(&tmp, fmt, args);
 
-	fprintf(stderr, "%s %s:%d:%s() %s\n",
-		log_level_to_string(level),
-		file, line, function,
-		tmp);
+	if (level <= 1) {
+#ifdef SYSLOG
+		syslog(log_level_to_syslog(level), "%s", tmp);
+#endif
+		fprintf(stderr, "%s: %s: %s\n",
+			log_level_to_string(level), function, tmp);
+	}
+#ifdef DEBUG
+	else if (level == 2)
+		fprintf(stderr, "%s: %s: %s\n",
+			log_level_to_string(level), function, tmp);
+	else if (level == 3)
+		fprintf(stderr, "%s: %s:%s(%u): %s\n",
+			log_level_to_string(level), file, function, line, tmp);
+#endif
 
 	free(tmp);
 
--- log.h
+++ log.h
@@ -1,7 +1,8 @@
 /*
- * Copyright (C) 2009 Nokia Corporation.
+ * Copyright (C) 2009-2010 Felipe Contreras
+ * Copyright (C) 2009-2010 Nokia Corporation
  *
- * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ * Author: Felipe Contreras <felipe.contreras@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,21 +23,30 @@
 #ifndef LOG_H
 #define LOG_H
 
-extern int debug_level;
+/* #define DEBUG */
 
-void
-pr_helper(unsigned int level,
-	  const char *file,
-	  const char *function,
-	  unsigned int line,
-	  const char *fmt,
-	  ...);
+#ifdef DEBUG
+extern int debug_level;
+#endif
+
+void pr_helper(unsigned int level,
+	       const char *file,
+	       const char *function,
+	       unsigned int line,
+	       const char *fmt,
+	       ...);
 
 #define pr_base(level, ...) pr_helper(level, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
 #define pr_err(...) pr_base(0, __VA_ARGS__)
 #define pr_warning(...) pr_base(1, __VA_ARGS__)
+
+#ifdef DEBUG
 #define pr_info(...) pr_base(2, __VA_ARGS__)
 #define pr_debug(...) pr_base(3, __VA_ARGS__)
+#else
+#define pr_info(...) ({ if (0) pr_base(2, __VA_ARGS__); })
+#define pr_debug(...) ({ if (0) pr_base(3, __VA_ARGS__); })
+#endif
 
 #endif /* LOG_H */
